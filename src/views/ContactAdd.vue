a0f81cb78019727ae4a6364e5cad00c5969936ad
b2007220 <127931507+b2007220@users.noreply.github.com>
2023-10-24 23:57:04 +0700

    addContact

--- src/views/ContactAdd.vue
+++ src/views/ContactAdd.vue
@@ -1,10 +1,11 @@
 <template>
   <div v-if="contact" class="page">
-    <h4>Hiệu chỉnh Liên hệ</h4>
+    <h4>Thêm Liên hệ</h4>
     <ContactForm :contact="contact" @submit:contact="addContact" />
     <p>{{ message }}</p>
   </div>
 </template>
+
 <script>
 import ContactForm from "@/components/ContactForm.vue";
 import ContactService from "@/services/contact.service";
@@ -12,44 +13,21 @@ export default {
   components: {
     ContactForm,
   },
-  props: {
-    id: { type: String, required: true },
-  },
   data() {
     return {
-      contact: null,
+      contact: {},
       message: "",
     };
   },
   methods: {
-    async getContact(id) {
-      try {
-        this.contact = await ContactService.get(id);
-      } catch (error) {
-        console.log(error);
-        // Chuyển sang trang NotFound đồng thời giữ cho URL không đổi
-        this.$router.push({
-          name: "notfound",
-          params: {
-            pathMatch: this.$route.path.split("/").slice(1),
-          },
-          query: this.$route.query,
-          hash: this.$route.hash,
-        });
-      }
-    },
     async addContact(data) {
       try {
         await ContactService.create(data);
-        this.message = "Thêm Liên hệ thành công.";
+        this.message = "Liên hệ được thêm vào danh bạ thành công.";
       } catch (error) {
         console.log(error);
       }
     },
-  },
-  created() {
-    this.getContact(this.id);
-    this.message = "";
   },
 };
 </script>
